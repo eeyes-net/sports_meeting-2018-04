7382ffd6c53f2ebc9d665d1eac72704d559d8b98
Tanpipi <931418134@qq.com>
2018-04-11 23:03:46 +0800

    1.0.1.1

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -6,28 +6,39 @@
 
 @section('content')
     <h1 id="title">新建项目的页面</h1>
-
+    <!-- 你看下这些name,照着我们改 -->
     <form action="{{ route('games.store') }}">
         <div class="form-group">
-            <label for="name" class="col-sm-2 control-label">项目名称</label>
+            <label for="firstname" class="col-sm-2 control-label">项目名称</label>
             <div class="col-sm-10">
                 <input type="text" name="name" class="form-control" placeholder="请输入名字">
             </div>
         </div>
         <div class="form-group">
-            <label for="begins_at" class="col-sm-2 control-label">开始时间</label>
-            <div class="col-md-10">
-                <input type="datetime-local" name="begins_at" class="form-control" id="begins_at">
+            <label for="firstname" class="col-sm-2 control-label">日期</label>
+            <div class="col-sm-10">
+                <label class="radio-inline">
+                    <input type="radio" name="date" id="option1" value="option1">14日
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="date" id="option2" value="option2">15日
+                </label>
             </div>
         </div>
         <div class="form-group">
-            <label for="class" class="col-sm-2 control-label">田径赛</label>
+            <label for="firstname" class="col-sm-2 control-label">开始时间</label>
+            <div class="col-sm-10">
+                <input type="datetime-local" class="form-control" name="begins_at" placeholder="请输入名字">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="firstname" class="col-sm-2 control-label">田径赛</label>
             <div class="col-sm-10">
                 <label class="radio-inline">
-                    <input type="radio" name="track" id="option1" value="2">田赛
+                    <input type="radio" name="track" id="option1" value="option1">田赛
                 </label>
                 <label class="radio-inline">
-                    <input type="radio" name="track" id="option2" value="1">径赛
+                    <input type="radio" name="track" id="option2" value="option2">径赛
                 </label>
             </div>
         </div>
