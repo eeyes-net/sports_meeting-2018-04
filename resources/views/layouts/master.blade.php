222cbbc9b9ce44a966639c6748e6519fca6add2e
Tanpipi <931418134@qq.com>
2018-04-16 22:50:04 +0800

    ico图标，meta标签和一点css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','西安交通大学56届校运会')</title>
+    <meta name ="keywords" content=" 西安交通大学第五十六届运动会">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/css/common.css">
     @yield('resource')
