fe858f09d8b6d93d746aea10b96e80429c09dba2
Tanpipi <931418134@qq.com>
2018-04-20 08:09:59 +0800

    紧急改设计。。改版1.3

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,14 +1,14 @@
-<header class="navbar" style="background:#0066cc;height:96px;margin-top:12px;position:relative;">
-    <p  style="text-align:center;color:#fff;line-height:96px;font-size:6rem;;"> <a href="{{ route('/') }}" style="color:#fff;text-decoration:none;">校运会 </a>  </p>
+<header class="navbar" style="position:relative;">
+    <img src="/static/index/banner.png" alt="banner">
     @if(Auth::check())
         <form action="{{ route('logout') }}" method="post" style="position: absolute;top: 24px;right: 10%;">
             {{ csrf_field() }}
-            <button type="submit" class="btn btn-primary">退出</button>
+            <button type="submit" style="width:8rem;font-size:3rem;color:#fff;position: absolute;top: 24px;right: 10%;border-bottom:3px solid #fff;background: rgba(255, 0, 0, 0);border: 0;border-bottom:3px solid #fff;">退出</button>
         </form>
     @else
-        <a href="{{ route('login') }}" style="font-size:3rem;color:#fff;position: absolute;top: 24px;right: 10%;">登陆</a>
+        <a href="{{ route('login') }}" style="font-size:3rem;color:#fff;position: absolute;top: 24px;right: 10%;border-bottom:3px solid #fff;">登陆</a>
     @endif
     @can('admin')
-        <a href="{{ route('admin') }}" style="font-size:3rem;color:#fff;position: absolute;top: 24px;left: 10%;">后台管理</a>
+        <a href="{{ route('admin') }}" style="font-size:3rem;color:#fff;position: absolute;top: 48px;left: 10%;border-bottom:3px solid #fff;">后台管理</a>
     @endcan
 </header>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <a href="{{ route('news.index') }}" class="btn btn-primary">返回新闻页</a>
+    <a href="{{ route('/') }}" class="btn btn-primary">返回主页</a>
     <div id="news_content">
         <h1>{{ $news->title }}</h1>
         {!! $news->content !!}
